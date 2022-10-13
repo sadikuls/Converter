01c59a137d964a5c78b7b034195c9d377f17d8ad
shadikul.bari <sadikul.bari2209@gmail.com>
2022-10-14 01:35:00 +0600

    added viewmodel

---
+++ app/src/main/java/com/sadikul/currencyexchange/presentation/currencyconversion/CurrencyconversionViewmodel.kt
@@ -0,0 +1,165 @@
+package com.sadikul.currencyexchange.presentation.currencyconversion
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sadikul.currencyexchange.core.utils.Resource
+import com.sadikul.currencyexchange.data.remote.dto.Currency
+import com.sadikul.currencyexchange.domain.model.ConversionModel
+import com.sadikul.currencyexchange.domain.usecase.*
+import com.sadikul.currencyexchange.domain.usecase.CommissionFeeCalculatorUseCase.Companion.COMMISSION_RATE
+import com.sadikul.currencyexchange.domain.usecase.CommissionFeeCalculatorUseCase.Companion.MAX_FREE_CONVERSION
+import com.sadikul.currencyexchange.domain.usecase.CommissionFeeCalculatorUseCase.Companion.MAX_FREE_CONVERSION_AMOUNT
+import com.sadikul.currencyexchange.presentation.currencyconversion.states.*
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CurrencyconversionViewmodel @Inject constructor(
+    private val currenciesUseCase: GetCurrenciesUseCase,
+    private val balanceListUseCase: BalanceListUseCase,
+    private val balanceListGeneratorUseCase: BalanceListInitializeUseCase,
+    private val submitBalanceUseCase: StoreCalculatedBalanceUseCase,
+    private val commissionCalculatorUseCase: CommissionFeeCalculatorUseCase,
+    private val currencyConversionUseCase: CurrencyCalculatorUseCase,
+    private val saveFromCurrencyUseCase: SaveFromCurrencyUseCase,
+    private val getFromCurrencyUseCase: GetFromCurrencyUseCase,
+    private val saveToCurrencyUseCase: SaveToCurrencyUseCase,
+    private val getToCurrencyUseCase: GetToCurrencyUseCase,
+    private val validatorUseCase: CurrencyConversionValidatorUseCase,
+): ViewModel() {
+    private val _state = MutableStateFlow(CurrencyListState())
+    val currencyListState get() = _state.asStateFlow()
+
+    private val _balanceState = MutableStateFlow(BalanceListState())
+    val balanceListState get() = _balanceState.asStateFlow()
+
+    private val _conversionState = MutableStateFlow(ConversionCalculationState())
+    val conversionState get() = _conversionState.asStateFlow()
+
+    private val _submissionState = MutableSharedFlow<ConversionResultState>(replay = 0)
+    val submissionState get() = _submissionState.asSharedFlow()
+
+
+    private val _statusFlow = MutableSharedFlow<String>(replay = 0)
+    val statusFlow get() = _statusFlow.asSharedFlow()
+
+    private val _fromCurrencyNamestatusFlow = MutableStateFlow(CurrencyState())
+    val fromCurrencyNameStateFlow get() = _fromCurrencyNamestatusFlow.asStateFlow()
+
+    private val _toCurrencyNamestatusFlow = MutableStateFlow(CurrencyState())
+    val toCurrencyNameStateFlow get() = _toCurrencyNamestatusFlow.asStateFlow()
+
+
+/*    private val _fromCurrencyNameList = MutableStateFlow<List<String>>(mutableListOf())
+    val fromCurrencyNameList get() = _fromCurrencyNameList.asStateFlow()*/
+
+    val fromCurrencyNameList = balanceListState.map { data ->
+        data.balanceList.filter { it.balance > 0 }.map { it.currency }.sorted()
+    }.stateIn(viewModelScope,SharingStarted.WhileSubscribed(),null)
+
+    init {
+        getFromCurrency()
+        getToCurrency()
+        getCurrencies()
+        getBalances()
+    }
+
+    private fun getBalances() {
+        viewModelScope.launch(Dispatchers.IO) {
+            balanceListUseCase().onEach {
+                _balanceState.value = BalanceListState(it)
+            }.launchIn(this)
+        }
+    }
+
+    private fun getCurrencies() = viewModelScope.launch(Dispatchers.IO) {
+            currenciesUseCase(forceToGetRemoteData = true).onEach {
+                when (it) {
+                    is Resource.Loading -> {
+                        _state.value = CurrencyListState(isLoading = true)
+                    }
+                    is Resource.Error -> {
+                        _state.value = CurrencyListState(error = "Something went wrong.")
+                    }
+                    is Resource.Success -> {
+                        _state.value = CurrencyListState(isLoading = false)
+                        it.data.let {
+                            balanceListGeneratorUseCase(it)
+                            Log.w("CurrencyValue","success : "+it.size)
+                            _state.value = CurrencyListState(items = it)
+                        }
+                    }
+                }
+            }.launchIn(this)
+        }
+
+
+    fun convertCurrency(fromCurrency: String,toCurrency: String,amount: Double) = viewModelScope.launch(Dispatchers.IO) {
+        currencyConversionUseCase(fromCurrency,toCurrency,amount).collect {
+            when (it) {
+                is Resource.Error -> {
+                    _conversionState.value = ConversionCalculationState(conversionData = it.data,message = it.message)
+
+                }
+                is Resource.Success -> {
+                    _conversionState.value = ConversionCalculationState(conversionData = it.data)
+                }
+                is Resource.Loading -> {}
+            }
+        }
+    }
+
+    fun submit(data: ConversionModel) = viewModelScope.launch(Dispatchers.IO) {
+        data.let {
+            it.commission = commissionCalculatorUseCase(
+                data.fromAmount,
+                data.fromCurrency,
+                COMMISSION_RATE,
+                MAX_FREE_CONVERSION,
+                MAX_FREE_CONVERSION_AMOUNT
+            )
+            validatorUseCase(it).onEach { result ->
+                if (result.isValid) {
+                    submitBalanceUseCase(
+                        it
+                    )
+                    _submissionState.emit(
+                        ConversionResultState(
+                            conversionModel = it
+                        )
+                    )
+                } else {
+                    _statusFlow.emit(result.msg)
+                }
+            }.launchIn(this)
+        }
+    }
+
+    fun saveFromCurrency(fromCurrency: Currency) = viewModelScope.launch {
+        fromCurrency.apply {
+            saveFromCurrencyUseCase(this)
+            _fromCurrencyNamestatusFlow.value = CurrencyState(this)
+        }
+    }
+    fun saveToCurrency(toCurrency: Currency) = viewModelScope.launch {
+        toCurrency.apply {
+            saveToCurrencyUseCase(this)
+            _toCurrencyNamestatusFlow.value = CurrencyState(this)
+        }
+    }
+
+    private fun getFromCurrency() = viewModelScope.launch{
+        getFromCurrencyUseCase().let {
+            _fromCurrencyNamestatusFlow.value = CurrencyState(it)
+        }
+    }
+
+    private fun getToCurrency() = viewModelScope.launch {
+        getToCurrencyUseCase()?.let {
+            _toCurrencyNamestatusFlow.value = CurrencyState(it)
+        }
+    }
+}
